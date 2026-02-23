300926ca6f6d99240bd066ff1e0a38b3317c5c80
Egor <egoralexandrov131044@gmail.com>
2026-02-23 19:25:01 +0300

    Наглупил чуть чуть, там где были ошибки поменял местами req и res

--- practice_2/app.js
+++ practice_2/app.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000;
 
 let products = [
-    {id: 1, name: 'Предмет1', cost: 2400},
-    {id: 2, name: 'Предмет2', cost: 4200},
-    {id: 3, name: 'Предмет3', cost: 1300},
+    {id: 1, name: 'Открывашка', cost: 2400},
+    {id: 2, name: 'Закрывашка', cost: 4200},
+    {id: 3, name: 'Разбивашка', cost: 1300},
 ]
 
 app.use(express.json());
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
     console.log("Открыта Главная Страница");
 });
 
-app.post('/products', (res, req) => {
+app.post('/products', (req, res) => {
     const { name, cost } = req.body;
 
     const newProduct = {
@@ -28,11 +28,11 @@ app.post('/products', (res, req) => {
     res.statusCode(201).json(newProduct);
 });
 
-app.get('/products', (res, req) => {
+app.get('/products', (req, res) => {
     res.send(JSON.stringify(products));
 });
 
-app.get('/products/:id', (res, req) => {
+app.get('/products/:id', (req, res) => {
     let product = products.find(p => p.id == req.params.id);
     res.send(JSON.stringify(product));
 });
@@ -47,7 +47,7 @@ app.patch('/products/:id', (req, res) => {
     res.json(product);
 });
 
-app.delete('/users/:id', (res, req) => {
+app.delete('/users/:id', (req, res) => {
     products = products.filter(p => p.id != req.params.id);
     res.send('OK');
 });
